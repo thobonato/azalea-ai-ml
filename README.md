4635d9f1daa8908826db3d7becbd8a0bd139f357
Thomaz Bonato <thobonato@icloud.com>
2024-06-29 00:19:03 -0400

    push

---
+++ README.md
@@ -0,0 +1,3 @@
+NOTE: 
+
+must have .env with MISTRAL_API_KEY = "api_key_here"
--- huggingface.py
+++ huggingface.py
@@ -1,20 +1,27 @@
 from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage
+from dotenv import load_dotenv
 import os
 
-api_key = os.environ["MISTRAL_API_KEY"]
-model = "mistral-large-latest"
+load_dotenv()
 
-client = MistralClient(api_key=api_key)
+def prompt_mistral(prompt: str) -> str:
+    api_key = os.environ["MISTRAL_API_KEY"]
+    model = "open-mistral-7b"
 
-messages = [
-    ChatMessage(role="user", content="What is the best French cheese?")
-]
+    client = MistralClient(api_key=api_key)
 
-# No streaming
-chat_response = client.chat(
-    model=model,
-    messages=messages,
-)
+    messages = [
+        ChatMessage(role="user", content=prompt)
+    ]
 
-print(chat_response.choices[0].message.content)
+    # No streaming
+    chat_response = client.chat(
+        model=model,
+        messages=messages,
+    )
+
+    print(chat_response.choices[0].message.content)
+
+if __name__ == "__main__":
+    prompt_mistral("What is the best French cheese?")
